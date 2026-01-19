10d9f6190d04a8749d5051793a9f1c7ec027a58d
Omprakash Sah Kanu <opsworld.g@gmail.com>
2026-01-18 18:25:02 -0700

    fix: correct Vite entry file path in app.blade.php (jsx -> tsx)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
 </head>
 
