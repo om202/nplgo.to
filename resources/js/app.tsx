0ed108e854d8c37eb9a8f2cbdbeb566b5f9745f2
Omprakash Sah Kanu <opsworld.g@gmail.com>
2026-03-22 16:47:05 -0600

    fix performance

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,11 +1,10 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true }) as Record<string, { default: React.ComponentType }>;
-        return pages[`./Pages/${name}.tsx`];
-    },
+    resolve: name => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
